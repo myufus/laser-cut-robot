8c8b5b178d00992c79ef85fd5507c4e05ccd6f3f
Muhammad Yusuf <musufyay@gmail.com>
2016-10-25 18:29:48 -0400

    Bug fix
    How a copied version of a method got in there I'll never know

--- laserCutCad.groovy
+++ laserCutCad.groovy
@@ -160,11 +160,7 @@ return new ICadGenerator(){
 		//add(csg,moveDHValues(physLink,dh),dh.getListener())
 
 		double ServoKeepawayRad = Math.sqrt((servoReference.getMinX()*servoReference.getMinX())+(servoReference.getMaxY()*servoReference.getMaxY()))+1
-		CSG upperLink = toZMin(new Cylinder(ServoKeepawayRad,10,(int)8).toCSG())private CSG toZMin(CSG incoming,CSG target){
-
-		return incoming.transformed(new Transform().translateZ(-target.getBounds().getMin().z));
-
-	}
+		CSG upperLink = toZMin(new Cylinder(ServoKeepawayRad,10,(int)8).toCSG())
 		add(csg, moveDHValues(horn, dh), dh.getListener())
 		if(neck ==sourceLimb ){
 			
