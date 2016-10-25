192e91cab1a4588f7e024f3dcc0fd9fbb2ef945a
Muhammad Yusuf <musufyay@gmail.com>
2016-10-25 14:12:40 -0400

    Fixed servo horn appendage sizes

--- laserCutCad.groovy
+++ laserCutCad.groovy
@@ -156,7 +156,7 @@ return new ICadGenerator(){
 		}
 		add(csg,moveDHValues(horn,dh),dh.getListener())
 
-		CSG physLink = new Cube(200, 200, 10).toCSG()
+		CSG physLink = new Cube(25, 25, 25).toCSG()
 		add(csg,moveDHValues(physLink,dh),dh.getListener())
 
 		if(neck ==sourceLimb ){
